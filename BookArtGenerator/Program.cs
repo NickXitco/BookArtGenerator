217608f30daa4dff7027856431cf4fe0c0f28bb0
Nick Xitco <nxitco1@jhu.edu>
2019-12-22 01:59:01 -0500

    Progress Towards getting lines

--- BookArtGenerator/Program.cs
+++ BookArtGenerator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using iText.IO.Font.Constants;
@@ -31,41 +32,48 @@ namespace BookArtGenerator
             var document = new Document(new PdfDocument(new PdfWriter(dest)), new PageSize(1296, 1728));
             document.SetMargins(72, 72, 72 * 2, 72);
             var p =  new Paragraph();
-            p.SetTextAlignment(TextAlignment.JUSTIFIED_ALL);
+            //p.SetTextAlignment(TextAlignment.JUSTIFIED_ALL);
+            
+            var adjustedWidth = image.Width * (72 / image.HorizontalResolution);
+            var adjustedHeight = image.Height * (72 / image.VerticalResolution);
+            
+            var font = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
+
+            var runningTotalPixelCount = 0.0;
+            var bankedPixels = 0.0;
+            
             for (var i = 0; i < text.Length; i++) {
-                p.Add(Color(text[i], i, image));
+                Text t = new Text(text[i].ToString());
+
+                var pixelWidth = font.GetWidth(text[i], 5);
+
+                var roundedWidth = (int) Math.Round(pixelWidth + bankedPixels);
+                runningTotalPixelCount += pixelWidth;
+                bankedPixels = pixelWidth - roundedWidth;
+
+                if (runningTotalPixelCount > adjustedWidth)
+                {
+                    var s = "";
+                    for (var j = i - 25; j < i + 1; j++)
+                    {
+                        s += text[j].ToString();
+                    }
+                    Console.WriteLine(runningTotalPixelCount.ToString() + ": " + s);
+                    runningTotalPixelCount = 0;
+                }
+                
+                
+                var color = ConvertToCmyk(image.GetPixel(i % image.Width, i / image.Height));
+                
+                t.SetFontColor(iText.Kernel.Colors.Color.MakeColor(new PdfDeviceCs.Cmyk(), color));
+                t.SetFont(font);
+                t.SetFontSize(5);
+
+                p.Add(t);
             }
+            
             document.Add(p);
             document.Close();
-        }
-
-        private static Text Color(char c, int i, Bitmap image) {
-            Text t = new Text(c.ToString());
-
-            var color = ConvertToCmyk(image.GetPixel(i % image.Width, i / image.Height));
-            var f = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
-            
-            var pixelWidth = f.GetWidth(c, 5);
-
-            var flooredWidth = Math.Floor(pixelWidth);
-
-            var leftover = pixelWidth - flooredWidth;
-            
-            //TODO the idea here is that we keep a running total of "leftover" width, and whenever we check the pixelWidth
-            //of a character, we add the global leftovers to the pixelWidth, then floor it.
-            //
-            // e.g: let's say we have a letter with width 2.6. That gets floored to 2 pixels of the reference image
-            //with .6 pixels leftover. The next letter has width 1.5 pixels. That gets added to the leftovers giving
-            //it 2.1 pixels, which gets floored to grab 2 pixels of the reference image, instead of 1. Now we have
-            //.1 leftover pixels, and we repeat the process. This should ensure that we don't leave any pixels unaccounted
-            //for by the end of the loop for each row, or that the final character isn't getting overrepresented.
-            
-            
-            t.SetFont(f);
-            t.SetFontSize(5);
-            t.SetFontColor(iText.Kernel.Colors.Color.MakeColor(new PdfDeviceCs.Cmyk(), color));
-            
-            return t;
         }
 
         private static float[] ConvertToCmyk(Color color) {
