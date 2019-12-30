4108db957809ad10aa50732c3d549cc1b5104dc3
Nick Xitco <nxitco1@jhu.edu>
2019-12-30 11:42:39 -0800

    Working whitespace manager and rough commenting done
    Next step is UI

--- BookArtGenerator/Program.cs
+++ BookArtGenerator/Program.cs
@@ -25,9 +25,11 @@ namespace BookArtGenerator
 
         private const float FontSize = 5f;
         private const float Leading = 0.5f;
-        private const float LeadingCoef = 2.640625f;
+        private const float LeadingCoef = 2.640625f; //This coef was decided by trial and error, it has no basis to it.
         private const float PHeight = FontSize * Leading * LeadingCoef;
 
+        private const float LightnessThreshold = 0.9f;
+        
         public static void Main() {
             var file = new FileInfo(Dest);
             file.Directory?.Create();
@@ -42,15 +44,17 @@ namespace BookArtGenerator
             var document = new Document(new PdfDocument(new PdfWriter(dest)), new PageSize(Width, Height));
             document.SetMargins(Inch, Inch, Inch, Inch);
             var font = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
-            
-            image = new Bitmap(image, new Size((int) (image.Width * (Inch / image.HorizontalResolution)), (int) (image.Height * (Inch / image.VerticalResolution))));
+
+            image = new Bitmap(image,
+                new Size((int) (image.Width * (Inch / image.HorizontalResolution)),
+                        (int) (image.Height * (Inch / image.VerticalResolution)))); //Rescale image in case it's not 72dpi, may need to consider cropping it as well for generality's sake.
 
             var i = 0;
             var numLines = 0;
             var bankedHeight = 0.0;
 
             Console.WriteLine("Coloring text...");
-            while ((numLines + 1) * PHeight < TextHeight)
+            while ((numLines + 1) * PHeight < TextHeight) //+1 because we don't want to add a line that's gonna go over. if this looks bad we could consider adding a condition like line 72.
             {
                 var p = new Paragraph().SetMargin(0).SetPadding(0).SetWidth(Width - 2 * Inch).SetHeight(PHeight)
                     .SetTextAlignment(TextAlignment.JUSTIFIED_ALL);
@@ -58,44 +62,50 @@ namespace BookArtGenerator
                 var bankedWidth = 0.0;
                 
                 var roundedHeight = (int) Math.Round(PHeight + bankedHeight);
-                bankedHeight = (PHeight + bankedHeight) - roundedHeight;
+                bankedHeight = (PHeight + bankedHeight) - roundedHeight; //return unused bank to bank
 
                 while (i < text.Length)
                 {
-                    var pixelWidth = font.GetWidth(text[i], FontSize);
+                    var pixelWidth = font.GetWidth(text[i], FontSize); 
                     var roundedWidth = (int) Math.Round(pixelWidth + bankedWidth);
 
-                    if (runningWidth + pixelWidth > TextWidth) break;
+                    if (runningWidth + pixelWidth > TextWidth) break; //break condition for crossing over the right margin
+                    
+                    var startX = (int) Math.Floor(runningWidth);
+                    var startY = (int) Math.Floor(numLines * PHeight);
+                    var color = AverageColor(image, startX, startY, roundedWidth, roundedHeight); //run kernel for area size of character.
 
-                    float[] color = {0, 0, 0, 0};
-                    
-                    if (roundedWidth > 0) // this isn't getting triggered like,,, 50% of the time lol
+                    if (ColorIsTooLight(color, LightnessThreshold) && !string.IsNullOrWhiteSpace(text[i].ToString()))
                     {
-                        color = AverageColor(image, (int) Math.Floor(runningWidth),
-                            (int) Math.Floor(numLines * PHeight), roundedWidth,
-                            roundedHeight);
+                        text = text.Insert(i, " "); //whitespace handling so we don't print characters that you can't read
+                        continue;
                     }
-                    
+
                     runningWidth += pixelWidth;
-                    bankedWidth = (pixelWidth + bankedWidth) - roundedWidth;
+                    bankedWidth = (pixelWidth + bankedWidth) - roundedWidth; //return unused pixels to bank.
 
                     var t = new Text(text[i].ToString());
-                    t.SetFontColor(iText.Kernel.Colors.Color.MakeColor(new PdfDeviceCs.Cmyk(), color));
+                    t.SetFontColor(iText.Kernel.Colors.Color.MakeColor(new PdfDeviceCs.Cmyk(), ConvertToCmyk(color)));
                     t.SetFont(font);
                     t.SetFontSize(FontSize);
 
                     i++;
 
-                    p.Add(t);
+                    p.Add(t); //add character to single-line paragraph
                 }
                 numLines++;
-                document.Add(p);
+                document.Add(p); //add single-line paragraph to document
             }
             Console.WriteLine("Closing out...");
             document.Close();
         }
 
-        private static float[] AverageColor(Bitmap img, int startX, int startY, int kernelWidth, int kernelHeight) {
+        private static bool ColorIsTooLight(Color color, float threshold)
+        {
+            return color.GetBrightness() >= threshold; //method returns 0 black to 1 white
+        }
+        
+        private static Color AverageColor(Bitmap img, int startX, int startY, int kernelWidth, int kernelHeight) {
             var r = 0;
             var b = 0;
             var g = 0;
@@ -112,13 +122,16 @@ namespace BookArtGenerator
             }
 
             var numPixels = kernelWidth * kernelHeight;
-            r /= numPixels;
-            b /= numPixels;
+            if (numPixels < 1) return Color.White; //likely-unused edge-case handler in case a zero-width character goes through 
+            
+            r /= numPixels; //post-fact averaging, maybe consider making r, g, b larger values or do moving average
+            b /= numPixels; //we just don't want to overflow the int if the kernel is too large
             g /= numPixels;
             
-            return ConvertToCmyk(Color.FromArgb(r, g, b));
+            return Color.FromArgb(r, g, b);
         }
 
+        // standard RGB->CMYK conversion for print
         private static float[] ConvertToCmyk(Color color) {
             var r = (float) (color.R / 255.0);
             var g = (float) (color.G / 255.0);
