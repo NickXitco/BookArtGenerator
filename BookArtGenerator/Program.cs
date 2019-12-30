b270d261bec15e68be3df6d65a067279072b2575
Nick Xitco <nxitco1@jhu.edu>
2019-12-29 21:25:42 -0800

    Semi-working coloring function

--- BookArtGenerator/Program.cs
+++ BookArtGenerator/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using iText.IO.Font.Constants;
@@ -8,11 +7,8 @@ using iText.Kernel.Font;
 using iText.Kernel.Geom;
 using iText.Kernel.Pdf;
 using iText.Kernel.Pdf.Colorspace;
-using iText.Layout.Borders;
 using iText.Layout.Element;
-using iText.Layout.Layout;
 using iText.Layout.Properties;
-using iText.StyledXmlParser.Jsoup.Nodes;
 using Color = System.Drawing.Color;
 using Document = iText.Layout.Document;
 
@@ -20,11 +16,12 @@ namespace BookArtGenerator
 {
     internal static class Program
     {
-        private const string Dest = "../../../hello_world_height_compare.pdf";
+        private const string Dest = "../../../juno.pdf";
         private const int Width = 18 * Inch;
         private const int Height = 24 * Inch;
         private const int Inch = 72;
 
+        private const int TextWidth = Width - 2 * Inch;
         private const int TextHeight = Height - 3 * Inch;
         
         private const float FontSize = 5f;
@@ -32,7 +29,7 @@ namespace BookArtGenerator
         private const float LeadingCoef = 2.640625f;
         private const float PHeight = FontSize * Leading * LeadingCoef;
 
-        public static void Main(string[] args) {
+        public static void Main() {
             var file = new FileInfo(Dest);
             file.Directory?.Create();
             
@@ -45,36 +42,44 @@ namespace BookArtGenerator
         private static void CreatePdf(string dest, string text, Bitmap image) {
             var document = new Document(new PdfDocument(new PdfWriter(dest)), new PageSize(Width, Height));
             document.SetMargins(Inch, Inch, Inch, Inch);
-            var adjustedWidth = image.Width * (Inch / image.HorizontalResolution);
-            var adjustedHeight = image.Height * (Inch / image.VerticalResolution);
             var font = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
 
             var i = 0;
             var numLines = 0;
+            var bankedHeight = 0.0;
 
+            
             Console.WriteLine("Coloring text...");
             while ((numLines + 1) * PHeight < TextHeight)
             {
-                var p = new Paragraph().SetMargin(0).SetPadding(0).SetWidth(Width - 2 * Inch).SetHeight(PHeight).SetTextAlignment(TextAlignment.JUSTIFIED_ALL).SetBackgroundColor(DeviceRgb.GREEN);
+                var p = new Paragraph().SetMargin(0).SetPadding(0).SetWidth(Width - 2 * Inch).SetHeight(PHeight)
+                    .SetTextAlignment(TextAlignment.JUSTIFIED_ALL);
                 var runningWidth = 0.0;
-                var bankedPixels = 0.0;
+                var bankedWidth = 0.0;
+                
+                var roundedHeight = (int) Math.Round(PHeight + bankedHeight);
+                bankedHeight = PHeight - roundedHeight;
 
                 while (i < text.Length)
                 {
-                    Text t = new Text(text[i].ToString());
-
                     var pixelWidth = font.GetWidth(text[i], FontSize);
+                    var roundedWidth = (int) Math.Round(pixelWidth + bankedWidth);
 
-                    var roundedWidth = (int) Math.Round(pixelWidth + bankedPixels);
+                    if (runningWidth + pixelWidth > TextWidth) break;
 
-                    if (runningWidth + pixelWidth > adjustedWidth) break;
-
+                    float[] color = {1, 0, 0, 0};
+                    
+                    if (roundedWidth > 0) // this isn't getting triggered like,,, 50% of the time lol
+                    {
+                        color = AverageColor(image, (int) Math.Floor(runningWidth),
+                            (int) Math.Floor(numLines * PHeight), roundedWidth,
+                            roundedHeight); //idk how to use this function lol
+                    }
+                    
                     runningWidth += pixelWidth;
+                    bankedWidth = pixelWidth - roundedWidth;
 
-                    bankedPixels = pixelWidth - roundedWidth;
-
-                    var color = ConvertToCmyk(image.GetPixel(i % image.Width, i / image.Height));
-
+                    Text t = new Text(text[i].ToString());
                     t.SetFontColor(iText.Kernel.Colors.Color.MakeColor(new PdfDeviceCs.Cmyk(), color));
                     t.SetFont(font);
                     t.SetFontSize(FontSize);
@@ -83,20 +88,14 @@ namespace BookArtGenerator
 
                     p.Add(t);
                 }
-
-                var s = "";
-                for (var j = i - 25; j < i; j++)
-                {
-                    s += text[j].ToString();
-                }
-
                 numLines++;
                 document.Add(p);
             }
-            
-            var spacer = new Paragraph().SetWidth(Width).SetHeight(TextHeight - (numLines * PHeight)).SetMargin(0).SetPadding(0).SetBackgroundColor(DeviceRgb.BLUE);
-            
-            var titleBox = new Paragraph().SetWidth(Width).SetHeight(Inch).SetMargin(0).SetPadding(0).SetBackgroundColor(DeviceRgb.RED);
+
+            var spacer = new Paragraph().SetWidth(Width).SetHeight(TextHeight - (numLines * PHeight)).SetMargin(0)
+                .SetPadding(0);
+
+            var titleBox = new Paragraph().SetWidth(Width).SetHeight(Inch).SetMargin(0).SetPadding(0);
             var titleFont = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
             
             Text title = new Text("JUNO");
@@ -111,6 +110,31 @@ namespace BookArtGenerator
             document.Close();
         }
 
+        private static float[] AverageColor(Bitmap img, int startX, int startY, int kernelWidth, int kernelHeight) {
+            var r = 0;
+            var b = 0;
+            var g = 0;
+            
+            for (var i = startX; i < startX + kernelWidth; i++)
+            {
+                for (var j = startY; j < startY + kernelHeight; j++)
+                {
+                    var color = img.GetPixel(i, j);
+                    r += color.R;
+                    b += color.B;
+                    g += color.G;
+                }
+            }
+
+            var numPixels = kernelWidth * kernelHeight;
+            r /= numPixels;
+            b /= numPixels;
+            g /= numPixels;
+            
+            return ConvertToCmyk(Color.FromArgb(r, g, b));
+        }
+        
+        
         private static float[] ConvertToCmyk(Color color) {
             var r = (float) (color.R / 255.0);
             var g = (float) (color.G / 255.0);
@@ -122,13 +146,13 @@ namespace BookArtGenerator
             float m = 0;
             float y = 0;
 
-            if ((Math.Abs(k - 1) < 0.001)) return new float[4] {c, m, y, k};
+            if ((Math.Abs(k - 1) < 0.001)) return new[] {c, m, y, k};
             
             c = (1 - r - k) / (1 - k);
             m = (1 - g - k) / (1 - k);
             y = (1 - b - k) / (1 - k);
 
-            return new float[4] {c, m, y, k};
+            return new[] {c, m, y, k};
         }
     }
 }
